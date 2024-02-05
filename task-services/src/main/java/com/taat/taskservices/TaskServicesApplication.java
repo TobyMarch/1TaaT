e9fcf14cc9b0e83ae1f6a1a70c862acb9e442eff
TobyMarch <{ID}+{username}@users.noreply.github.com>
2024-02-05 18:24:20 -0500

    basic layout and java core

---
+++ task-services/src/main/java/com/taat/taskservices/TaskServicesApplication.java
@@ -0,0 +1,13 @@
+package com.taat.taskservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskServicesApplication.class, args);
+	}
+
+}
---
+++ task-services/src/test/java/com/taat/taskservices/TaskServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taat.taskservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
