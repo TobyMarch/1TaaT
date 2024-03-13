e187f7d5317aaac8a13a922d7f689e98910dade6
wenqifu <wenqifu23@email.com>
2024-03-13 19:10:38 -0400

    Initial commit, delete task and archive task

--- task-services/src/main/java/com/taat/taskservices/controllers/TaskController.java
+++ task-services/src/main/java/com/taat/taskservices/controllers/TaskController.java
@@ -1,21 +1,15 @@
 package com.taat.taskservices.controllers;
 
-import java.util.List;
-
+import com.taat.taskservices.model.Task;
+import com.taat.taskservices.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.taat.taskservices.model.Task;
-import com.taat.taskservices.services.TaskService;
-
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -36,5 +30,28 @@ public class TaskController {
   public ResponseEntity<?> addNewTasks(@RequestBody List<Task> tasks) {
     taskService.createUpdateTasks(tasks).subscribe();
     return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+
+  // delete task
+  @CrossOrigin(origins = { "http://localhost:3000", "https://onetaat-web.onrender.com" })
+  @DeleteMapping("/tasks/{id}")
+  public Mono<ResponseEntity<Void>> deleteTask(@PathVariable String id) {
+    return taskService.deleteById(id)
+      .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
+      .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+  }
+
+  // archive task
+  @CrossOrigin(origins = { "http://localhost:3000", "https://onetaat-web.onrender.com" })
+  @PostMapping("/tasks/{id}/archive")
+  public Mono<ResponseEntity<Task>> archiveTask(@PathVariable String id) {
+    return taskRepository.findById(id)
+      .flatMap(task -> {
+        task.setArchived(true); 
+        return taskRepository.save(task);
+      })
+      .map(updatedTask -> new ResponseEntity<>(updatedTask, HttpStatus.OK))
+      .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 }
