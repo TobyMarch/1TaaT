006eed9d02be3fa72fd2b656bd99ff313f3040b5
Toby March <2407561+TobyMarch@users.noreply.github.com>
2024-03-27 13:26:58 -0400

    Prioritization api integration (#22)
    * logger config for local builds
    
    * Initial Flux implementation
    
    * Task POST/PUT sort fix and task GET logic
    
    * delete join records when task is deleted
    
    * task archive logic
    
    * remove test method
    
    * more test resource deletion
    
    ---------
    
    Co-authored-by: TobyMarch <{ID}+{username}@users.noreply.github.com>

--- task-services/src/main/java/com/taat/taskservices/services/TaskService.java
+++ task-services/src/main/java/com/taat/taskservices/services/TaskService.java
@@ -2,9 +2,12 @@ package com.taat.taskservices.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -12,7 +15,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.taat.taskservices.model.Task;
+import com.taat.taskservices.model.UserTask;
 import com.taat.taskservices.repository.TaskRepository;
+import com.taat.taskservices.repository.UserTaskRepository;
 import com.taat.taskservices.services.comparators.TaskDelayableComparator;
 import com.taat.taskservices.services.comparators.TaskDueDateComparator;
 import com.taat.taskservices.services.comparators.TaskPriorityComparator;
@@ -26,8 +31,13 @@ import reactor.core.publisher.Mono;
 @Service
 public class TaskService {
 
+    private final Logger logger = LoggerFactory.getLogger(TaskService.class);
+
     @Autowired
     private TaskRepository taskRepository;
+
+    @Autowired
+    private UserTaskRepository userTaskRepository;
 
     public Flux<Task> getPrioritizedTasks() {
         Sort priority = Sort.by(Sort.Direction.DESC, "priority");
@@ -44,12 +54,79 @@ public class TaskService {
         return taskRepository.count();
     }
 
-    public Flux<Task> createUpdateTasks(final List<Task> tasks) {
-        return taskRepository.insert(tasks);
+    public Mono<Task> getTopTask(final String userId) {
+        return userTaskRepository.findTopTaskByUserTaskSort(userId);
+    }
+
+    public Mono<List<Task>> createUpdateTasks(final List<Task> tasks) {
+        try {
+            return taskRepository.saveAll(tasks)
+                    .doOnNext(savedTask -> {
+                        userTaskRepository.findUserTaskByUserIdTaskId("", savedTask.getId())
+                                .hasElement().subscribe(joinRecordExists -> {
+                                    // Create a new db entry linking the task to the user if none exists
+                                    if (!joinRecordExists) {
+                                        logger.info(
+                                                String.format("Inserting record for task: %s", savedTask.getTitle()));
+                                        UserTask joinEntry = new UserTask();
+                                        joinEntry.setUserId("");
+                                        joinEntry.setTaskId(savedTask.getId());
+                                        userTaskRepository.insert(joinEntry).subscribe(insertedTask -> {
+                                            logger.debug(
+                                                    String.format("Insertion result for task: %s",
+                                                            insertedTask.toString()));
+                                        });
+                                    }
+                                });
+                    }).collectList().doAfterTerminate(() -> {
+                        List<Task> unsortedTasks = new ArrayList<>();
+                        List<UserTask> sortedJoinRecords = new ArrayList<>();
+                        logger.info("Insertion complete, starting sort operation...");
+                        // Select all tasks connected to the user
+                        userTaskRepository.findByUserId("")
+                                .collectList().doOnSuccess((joinList) -> {
+                                    for (UserTask joinRecord : joinList) {
+                                        logger.debug(String.format("Found join record: %s", joinRecord.toString()));
+                                        Task taskRecord = taskRepository.findById(joinRecord.getTaskId()).block();
+                                        if (taskRecord != null) {
+                                            logger.debug(
+                                                    String.format("Corresponding Task: %s", taskRecord.toString()));
+                                            unsortedTasks.add(taskRecord);
+                                        }
+                                    }
+                                    // Priority-Sort tasks then apply sorting values to UserTask records
+                                    double currentPriorityValue = unsortedTasks.size();
+                                    logger.info(String.format("Tasks to sort: %d", unsortedTasks.size()));
+                                    for (Task sortedTask : prioritySortTasks(unsortedTasks)) {
+                                        Optional<UserTask> userTaskOptional = joinList.stream()
+                                                .filter(ut -> ut.getTaskId().equals(sortedTask.getId())).findFirst();
+                                        if (userTaskOptional.isPresent()) {
+                                            UserTask userTask = userTaskOptional.get();
+                                            userTask.setSortValue(currentPriorityValue--);
+                                            sortedJoinRecords.add(userTask);
+                                        }
+                                    }
+                                    logger.debug(String.format("Saving %d records", sortedJoinRecords.size()));
+                                    // update UserTask records
+                                    List<UserTask> output = userTaskRepository.saveAll(sortedJoinRecords).collectList()
+                                            .block();
+                                    if (output != null) {
+                                        for (UserTask userTask : output) {
+                                            logger.debug(String.format("Saved join record: %s", userTask.toString()));
+                                        }
+                                    }
+                                }).subscribe();
+                    });
+        } catch (Exception e) {
+            logger.error("Exception in save/update", e);
+        }
+        return null;
     }
 
     public Mono<Void> deleteById(String id) {
-        return taskRepository.deleteById(id);
+        return taskRepository.deleteById(id).doOnSuccess(success -> {
+            userTaskRepository.deleteByTaskId(id).blockLast();
+        });
     }
 
     public Mono<Task> archiveTask(String id) {
@@ -57,6 +134,16 @@ public class TaskService {
                 .flatMap(task -> {
                     task.setArchived(true);
                     return taskRepository.save(task);
+                }).doOnSuccess(savedTask -> {
+                    userTaskRepository.findByTaskId(id).map(Optional::of).defaultIfEmpty(Optional.empty())
+                            .flatMap(optionalUserTask -> {
+                                if (optionalUserTask.isPresent()) {
+                                    UserTask userTaskRecord = optionalUserTask.get();
+                                    userTaskRecord.setArchived(true);
+                                    userTaskRepository.save(userTaskRecord).block();
+                                }
+                                return Mono.empty();
+                            }).subscribe();
                 });
     }
 
--- task-services/src/test/java/com/taat/taskservices/services/TaskServiceTest.java
+++ task-services/src/test/java/com/taat/taskservices/services/TaskServiceTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -16,7 +17,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import com.taat.taskservices.model.Task;
+import com.taat.taskservices.model.UserTask;
 import com.taat.taskservices.repository.TaskRepository;
+import com.taat.taskservices.repository.UserTaskRepository;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -28,20 +31,43 @@ public class TaskServiceTest {
     @Mock
     TaskRepository taskRepo;
 
+    @Mock
+    UserTaskRepository userTaskRepo;
+
     @InjectMocks
     TaskService taskService;
 
     @Test
     public void testCreateUpdateTasks() {
-        Task testTask = new Task("1", "testOwner", "Test Task", "A task for testing", null, null, null, 5,
-                false, false);
-        Flux<Task> taskFlux = Flux.just(testTask);
-        Mockito.when(taskRepo.insert(Mockito.anyIterable())).thenReturn(taskFlux);
+        List<Task> taskList = getTestTasks();
+
+        Flux<Task> taskFlux = Flux.fromIterable(taskList);
+        Flux<UserTask> joinFlux = Flux.fromIterable(getTestTaskJoinEntries());
+        Mockito.when(taskRepo.saveAll(Mockito.anyIterable())).thenReturn(taskFlux);
+        Mockito.when(userTaskRepo.findUserTaskByUserIdTaskId(Mockito.anyString(), Mockito.anyString()))
+                .thenReturn(Mono.empty());
+        Mockito.when(userTaskRepo.findByUserId(Mockito.anyString()))
+                .thenReturn(joinFlux);
+        Mockito.when(userTaskRepo.insert(Mockito.any(UserTask.class))).thenReturn(Mono.just(new UserTask()));
+        // TODO find a better way to map input to mocked output
+        Mockito.when(taskRepo.findById(ArgumentMatchers.eq("1"))).thenReturn(Mono.just(taskList.get(0)));
+        Mockito.when(taskRepo.findById(ArgumentMatchers.eq("2"))).thenReturn(Mono.just(taskList.get(1)));
+        Mockito.when(taskRepo.findById(ArgumentMatchers.eq("3"))).thenReturn(Mono.just(taskList.get(2)));
+        Mockito.when(taskRepo.findById(ArgumentMatchers.eq("4"))).thenReturn(Mono.just(taskList.get(3)));
+        Mockito.when(taskRepo.findById(ArgumentMatchers.eq("5"))).thenReturn(Mono.just(taskList.get(4)));
+        Mockito.when(userTaskRepo.saveAll(ArgumentMatchers.anyList())).thenReturn(joinFlux);
 
         List<Task> inputList = new ArrayList<>();
-        inputList.add(testTask);
-        taskService.createUpdateTasks(inputList);
-        Mockito.verify(taskRepo, Mockito.times(1)).insert(Mockito.anyIterable());
+        inputList.addAll(taskList);
+        StepVerifier.create(taskService.createUpdateTasks(inputList))
+                .expectNext(taskList)
+                .verifyComplete();
+
+        Mockito.verify(taskRepo, Mockito.times(1)).saveAll(Mockito.anyIterable());
+        Mockito.verify(userTaskRepo, Mockito.times(5)).insert(Mockito.any(UserTask.class));
+        Mockito.verify(userTaskRepo, Mockito.times(1)).findByUserId(Mockito.anyString());
+        Mockito.verify(taskRepo, Mockito.times(5)).findById(Mockito.anyString());
+        Mockito.verify(userTaskRepo, Mockito.times(1)).saveAll(Mockito.anyIterable());
     }
 
     @Test
@@ -83,11 +109,13 @@ public class TaskServiceTest {
     public void testDeleteById() {
         String taskId = "1";
         Mockito.when(taskRepo.deleteById(taskId)).thenReturn(Mono.empty());
+        Mockito.when(userTaskRepo.deleteByTaskId(taskId)).thenReturn(Flux.empty());
 
         Mono<Void> result = taskService.deleteById(taskId);
 
         StepVerifier.create(result).verifyComplete();
         Mockito.verify(taskRepo, Mockito.times(1)).deleteById(taskId);
+        Mockito.verify(userTaskRepo, Mockito.times(1)).deleteByTaskId(taskId);
     }
 
     @Test
@@ -97,6 +125,9 @@ public class TaskServiceTest {
         
         Mockito.when(taskRepo.findById(taskId)).thenReturn(Mono.just(testTask));
         Mockito.when(taskRepo.save(testTask)).thenAnswer(invocation -> Mono.just(invocation.getArgument(0)));
+        Mockito.when(userTaskRepo.findByTaskId(taskId)).thenReturn(Flux.just(getTestTaskJoinEntries().get(0)));
+        Mockito.when(userTaskRepo.save(Mockito.any(UserTask.class)))
+                .thenReturn(Mono.just(getTestTaskJoinEntries().get(0)));
 
         Mono<Task> result = taskService.archiveTask(taskId);
 
@@ -107,6 +138,8 @@ public class TaskServiceTest {
 
         Mockito.verify(taskRepo, Mockito.times(1)).findById(taskId);
         Mockito.verify(taskRepo, Mockito.times(1)).save(testTask);
+        Mockito.verify(userTaskRepo, Mockito.times(1)).findByTaskId(taskId);
+        Mockito.verify(userTaskRepo, Mockito.times(1)).save(Mockito.any(UserTask.class));
     }
 
 
@@ -129,10 +162,19 @@ public class TaskServiceTest {
                 new Task("4", "testOwner", "Test Task 4", "A task that was due yesterday", null, null,
                         previousDateString + "T09:15:00", 5, false, false));
         taskList.add(
-                new Task("4", "testOwner", "Test Task 5", "A task that is due tomorrow", null, null,
+                new Task("5", "testOwner", "Test Task 5", "A task that is due tomorrow", null, null,
                         futureDateString + "T14:15:00", 5, false, false));
 
         return taskList;
+    }
+
+    private List<UserTask> getTestTaskJoinEntries() {
+        int index = 0;
+        List<UserTask> joinEntries = new ArrayList<>();
+        for (Task task : getTestTasks()) {
+            joinEntries.add(new UserTask(Integer.toString(index++), "", task.getId(), null, null, null, false));
+        }
+        return joinEntries;
     }
 
 }
