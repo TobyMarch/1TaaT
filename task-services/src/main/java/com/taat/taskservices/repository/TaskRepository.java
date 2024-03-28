16e3cb26e30a21e3b4a1e645bbdd35a0937b901e
TobyMarch <{ID}+{username}@users.noreply.github.com>
2024-03-28 13:34:38 -0400

    Reactive repo class cleanup

--- task-services/src/main/java/com/taat/taskservices/repository/TaskRepository.java
+++ task-services/src/main/java/com/taat/taskservices/repository/TaskRepository.java
@@ -1,18 +1,17 @@
-// package com.taat.taskservices.repository;
+package com.taat.taskservices.repository;
 
-// import com.taat.taskservices.model.Task;
+import com.taat.taskservices.model.Task;
 
-// import reactor.core.publisher.Flux;
-// import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-// import org.springframework.data.domain.Pageable;
-// import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-// import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
 // @Repository
-// public interface TaskRepository extends ReactiveMongoRepository<Task, String>
-// {
-// Flux<Task> findAllBy(Pageable pageable);
+public interface TaskRepository extends ReactiveMongoRepository<Task, String> {
+    Flux<Task> findAllBy(Pageable pageable);
 
-// Mono<Long> count();
-// }
+    Mono<Long> count();
+}
--- task-services/src/main/java/com/taat/taskservices/repository/UserTaskRepository.java
+++ task-services/src/main/java/com/taat/taskservices/repository/UserTaskRepository.java
@@ -1,38 +1,34 @@
-// package com.taat.taskservices.repository;
+package com.taat.taskservices.repository;
 
-// import org.springframework.data.mongodb.repository.Aggregation;
-// import org.springframework.data.mongodb.repository.Query;
-// import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-// import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
-// import com.taat.taskservices.model.Task;
-// import com.taat.taskservices.model.UserTask;
+import com.taat.taskservices.model.Task;
+import com.taat.taskservices.model.UserTask;
 
-// import reactor.core.publisher.Flux;
-// import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 // @Repository
-// public interface UserTaskRepository extends ReactiveMongoRepository<UserTask,
-// String> {
+public interface UserTaskRepository extends ReactiveMongoRepository<UserTask, String> {
 
-// @Aggregation(pipeline = { "{$match: {userId: '?0', archived: {$ne : true}
-// }}", "{$sort: { sortValue: -1 }}",
-// "{$addFields: {taskId: {$toObjectId: \"$taskId\"} }}",
-// "{$lookup: {from: \"tasksTest\", localField: \"taskId\", foreignField:
-// \"_id\", as: \"result\"}}",
-// "{$addFields: {result: {$first: \"$result\"}}}",
-// "{$match: {result: {$ne: null}}}", "{$limit: 1}", "{$replaceRoot: {newRoot:
-// \"$result\"}}" })
-// Mono<Task> findTopTaskByUserTaskSort(String userId);
+    @Aggregation(pipeline = { "{$match: {userId: '?0', archived: {$ne : true}}}", "{$sort: { sortValue: -1 }}",
+            "{$addFields: {taskId: {$toObjectId: \"$taskId\"} }}",
+            "{$lookup: {from: \"tasksTest\", localField: \"taskId\", foreignField:\"_id\", as: \"result\"}}",
+            "{$addFields: {result: {$first: \"$result\"}}}", "{$match: {result: {$ne: null}}}", "{$limit: 1}",
+            "{$replaceRoot: {newRoot:\"$result\"}}" })
+    Mono<Task> findTopTaskByUserTaskSort(String userId);
 
-// @Query("{userId: '?0', taskId: '?1'}")
-// Mono<UserTask> findUserTaskByUserIdTaskId(String userId, String taskId);
+    @Query("{userId: '?0', taskId: '?1'}")
+    Mono<UserTask> findUserTaskByUserIdTaskId(String userId, String taskId);
 
-// @Query("{userId: '?0'}")
-// Flux<UserTask> findByUserId(String userId);
+    @Query("{userId: '?0'}")
+    Flux<UserTask> findByUserId(String userId);
 
-// @Query("{taskId: '?0'}")
-// Flux<UserTask> findByTaskId(String taskId);
+    @Query("{taskId: '?0'}")
+    Flux<UserTask> findByTaskId(String taskId);
 
-// Flux<UserTask> deleteByTaskId(String taskId);
-// }
+    Flux<UserTask> deleteByTaskId(String taskId);
+}
