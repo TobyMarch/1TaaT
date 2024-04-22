a278b52dea09b0e33bb2fa07c58a71d62ba1ffcc
TobyMarch <{ID}+{username}@users.noreply.github.com>
2024-04-21 21:25:49 -0400

    AllowedMethods fix

--- task-services/src/main/java/com/taat/taskservices/config/CorsConfig.java
+++ task-services/src/main/java/com/taat/taskservices/config/CorsConfig.java
@@ -7,12 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins(
-            "http://localhost:3000",
-            "https://onetaat-web.onrender.com",
-            "https://onetaat.com"
-    ).allowCredentials(true);
-  }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(
+                "http://localhost:3000",
+                "https://onetaat-web.onrender.com",
+                "https://onetaat.com").allowCredentials(true).allowedMethods("*");
+    }
 }
