320076101f15e15606e19d6835f0ed1265105b7b
TobyMarch <{ID}+{username}@users.noreply.github.com>
2024-04-28 22:35:16 -0400

    refactor calendarEvent request payload

--- web-app/src/components/CalendarEvent.js
+++ web-app/src/components/CalendarEvent.js
@@ -2,19 +2,21 @@ import React, { useState } from 'react';
 
 export function CalendarEvent({event, handleSubmit}) {
     const [priority, setPriority] = useState(3);
+    const createdDate = event.created;
+    const startDate = event.start.dateTime ? event.start.dateTime : event.start.date;
     const dueDate = event.end.dateTime ? event.end.dateTime : event.end.date;
 
     const submitClicked = (e) => {
         e.preventDefault();
 
-        const currentDate = new Date().toISOString().split("T")[0];
-
         const task = {
-            owner: "",
+            externalId: event.id,
             title: event.summary,
-            createdDate: `${currentDate}T12:00`,
-            dueDate,
-            priority
+            createdDate: new Date(createdDate).toISOString(),
+            startDate: new Date(startDate).toISOString(),
+            dueDate: new Date(dueDate).toISOString(),
+            priority,
+            duration: "M"
         }
 
         handleSubmit(task);
