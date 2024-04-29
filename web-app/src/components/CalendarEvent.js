78f4ab369ac9f338577abed83c09db6376f5552f
TobyMarch <{ID}+{username}@users.noreply.github.com>
2024-04-29 12:06:00 -0400

    configurable duration

--- web-app/src/components/CalendarEvent.js
+++ web-app/src/components/CalendarEvent.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 export function CalendarEvent({event, handleSubmit}) {
     const [priority, setPriority] = useState(3);
+    const [duration, setDuration] = useState("M");
     const createdDate = event.created;
     const startDate = event.start.dateTime ? event.start.dateTime : event.start.date;
     const dueDate = event.end.dateTime ? event.end.dateTime : event.end.date;
@@ -16,7 +17,7 @@ export function CalendarEvent({event, handleSubmit}) {
             startDate: new Date(startDate).toISOString(),
             dueDate: new Date(dueDate).toISOString(),
             priority,
-            duration: "M"
+            duration
         }
 
         handleSubmit(task);
@@ -26,8 +27,10 @@ export function CalendarEvent({event, handleSubmit}) {
         <div>
             <h2>{event.summary}</h2>
             <p className="dueDate">
-                Due: {String(dueDate).split("T")[0]}<br/>
-                Priority: {priority}
+                Start: {String(startDate).split("T")[0]}<br />
+                End: {String(dueDate).split("T")[0]}<br />
+                Duration: {duration}<br />
+                Priority: {priority}<br />
             </p>
             <input
                 type="range"
@@ -36,7 +39,18 @@ export function CalendarEvent({event, handleSubmit}) {
                 min="1"
                 max="5"
                 onChange={(e) => setPriority(parseInt(e.target.value, 10))}
-              />
+            />
+            <div className="durationDropdown">
+                <select
+                    onChange={(e) => setDuration(e.target.value)}
+                    value={duration}
+                >
+                    <option value="S">Small</option>
+                    <option value="M">Medium</option>
+                    <option value="L">Large</option>
+                    <option value="XL">Extra Large</option>
+                </select>
+            </div>
             <button onClick={(e) => submitClicked(e)}>Add Task</button>
         </div>
     )
