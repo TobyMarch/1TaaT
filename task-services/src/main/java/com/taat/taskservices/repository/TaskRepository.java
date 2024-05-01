6ada1b0077a50bb3cf022b95633331e07deaf10a
TobyMarch <{ID}+{username}@users.noreply.github.com>
2024-04-30 21:45:13 -0400

    import cleanup

--- task-services/src/main/java/com/taat/taskservices/repository/TaskRepository.java
+++ task-services/src/main/java/com/taat/taskservices/repository/TaskRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.ExistsQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.taat.taskservices.dto.TaskDTO;
